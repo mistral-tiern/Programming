0bb9771fd39e896291b572482625fd25fca93d17
Llyneira <dawnieharless@gmail.com>
2022-05-01 11:21:00 -0700

    Found and fixed a bug where start tiles only randomized on screen refresh. Copied randomize start tiles function to doThing function so start tiles will be randomized on loading of page and on click. Separated out code to separate .js files to make code easier to read.

---
+++ spawns.js
@@ -0,0 +1,23 @@
+const spawns =
+[
+    {
+        name: "monster2",
+        filename: "monster2.png",
+        shape: ["cave", "path", "secret"],
+    },
+    {
+        name: "monster1",
+        filename: "monster1.png",
+        shape: ["water"],
+    },
+    {
+        name: "treasure",
+        filename: "treasure.png",
+        shape: ["cave", "secret", "path", "water"],
+    },
+    {
+        name: "trap",
+        filename: "trap.png",
+        shape: ["path", "cave", "secret", "water"],
+    }
+]
--- tiles.js
+++ tiles.js
@@ -1,4 +1,4 @@
-<script>
+
         const tiles =
         [
             {
@@ -488,4 +488,157 @@
             if(tile.shape.includes("deadend")) tile.weight=legendary
             if(tile.shape.includes("secret")) tile.weight=artifact
         }
-</script>
+
+        const temptiles = tiles.slice(0)
+
+        for (const tile of temptiles) {
+            tiles.push(
+                {
+                    ...tile,
+                    north: tile.west,
+                    south: tile.east,
+                    east: tile.north,
+                    west: tile.south,
+                    name: tile.name + "090",
+                    rotation: 90,
+                    mirror: 1,
+                }
+            )
+            tiles.push(
+                {
+                    ...tile,
+                    north: tile.south,
+                    south: tile.north,
+                    east: tile.west,
+                    west: tile.east,
+                    name: tile.name + "180",
+                    rotation: 180,
+                    mirror: 1,
+                }
+            )
+            tiles.push(
+                {
+                    ...tile,
+                    north: tile.east,
+                    south: tile.west,
+                    east: tile.south,
+                    west: tile.north,
+                    name: tile.name + "270",
+                    rotation: 270,
+                    mirror: 1,
+                }
+            )
+            tiles.push(
+                {
+                    ...tile,
+                    north: tile.north,
+                    south: tile.south,
+                    east: tile.west,
+                    west: tile.east,
+                    name: tile.name + "Mirror000",
+                    rotation: 0,
+                    mirror: -1,
+                }
+            )
+            tiles.push(
+                {
+                    ...tile,
+                    north: tile.west,
+                    south: tile.east,
+                    east: tile.south,
+                    west: tile.north,
+                    name: tile.name + "Mirror090",
+                    rotation: 90,
+                    mirror: -1,
+                }
+            )
+            tiles.push(
+                {
+                    ...tile,
+                    north: tile.south,
+                    south: tile.north,
+                    east: tile.east,
+                    west: tile.west,
+                    name: tile.name + "Mirror180",
+                    rotation: 180,
+                    mirror: -1,
+                }
+            )
+            tiles.push(
+                {
+                    ...tile,
+                    north: tile.east,
+                    south: tile.west,
+                    east: tile.north,
+                    west: tile.south,
+                    name: tile.name + "Mirror270",
+                    rotation: 270,
+                    mirror: -1,
+                }
+            )
+        }
+        const deck = {}
+        for (const tile of tiles) {
+            deck[tile.name] = tile
+        }
+    
+        const starttiles = tiles.filter(tile => tile.shape.includes("start"))
+        //console.log(starttiles)
+        randomize(starttiles)
+
+        function randomize(array) {
+            for (let step = 0; step < array.length; step++) {
+                const temporary = (array[step])
+                const randomindex = Math.floor(Math.random() * array.length)
+                array[step] = array[randomindex]
+                array[randomindex] = temporary
+            }
+        }
+        const coordToindex = (x, y, w, h) => {
+            if (x > w || x < 1) throw new Error("X coordinate is out of range.")
+            if (y > h || y < 1) throw new Error("Y coordinate is out of range.")
+            return ((y - 1) * w + (x - 1))
+        }
+        const arraylength = (w, h) => (w * h)
+        const result = coordToindex(8, 10, 8, 10)
+        let start = null
+        function doThing(w, h) {
+            start = Date.now()
+            randomize(starttiles)
+            if (w < 1) throw new Error("X is outside size constraints.")
+            if (h < 1) throw new Error("Y is outside size constraints.")
+            const map = new Array(w * h)
+            map.fill("")
+            const mapArray = buildMap(map, w, h)
+            console.log(mapArray)
+            const container = (document.getElementById("map"))
+            for (const child of Array.from(container.children)) {
+                child.remove()
+            }
+            const rowArray = []
+            let count = 0
+            let row = null
+            for (const tilename of mapArray) {
+                if (count % w === 0) {
+                    row = document.createElement("div")
+                    rowArray.push(row)
+    
+                }
+                count++
+                const tile = deck[tilename]
+                if (!tile) console.log("Tile not found. :(", tilename, deck)
+                const image = new Image(180, 180)
+                if (tile) {
+                    image.src = "newTiles/" + tile?.filename;
+                    image.style.cssText = `transform:scaleX(${tile.mirror}) rotate(${tile.rotation}deg)`;
+                    image.onclick = function () { console.log(tile) }
+                }
+                row.appendChild(image)
+            }
+            rowArray.reverse()
+            for (const ScoobySnack of rowArray) {
+                container.appendChild(ScoobySnack)
+            }
+    
+        }
+    
